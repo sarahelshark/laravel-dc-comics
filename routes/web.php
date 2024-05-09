446e7ce5c1cefa0189fc6197925ad880005236ca
sarahelshark <sarahelsh183@gmail.com>
2024-05-09 18:29:06 +0200

    step 1
    $ php artisan make:model Post -crm  + $ php artisan route:list

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+use PHPUnit\Framework\Attributes\PostCondition;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('post', PostController::class);
+//per la nostra identita post vogliamo creare 7 rotte differenti
