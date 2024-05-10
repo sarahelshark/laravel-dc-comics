b956539e4a1bbd7ba24e237689c91ecc5b859449
sarahelshark <sarahelsh183@gmail.com>
2024-05-10 11:52:57 +0200

    fix
    index() show()

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,26 +1,28 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body >
-   
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <h1>Hello Admin</h1><a href="#">view post</a>
-   
-    <!-- @dd($posts)
-    -->
-    <div> 
-        <h2>
+        <title>Posts List</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    </head>
+    <body class="antialiased">
+        
+        <h1>Hello Admin! Check the following posts</h1>
+
+        <div>
+         @foreach ($posts as $post)
+         <h2> 
             {{$post->title}}
-        </h2>
-        
-        
-    </div>
-
-    
-
-</body>
+         </h2>
+         <a href="{{route('posts.show',$post)}}">go to "{{$post->title}}"</a>
+         @endforeach
+         
+        </div>
+    </body>
 </html>
